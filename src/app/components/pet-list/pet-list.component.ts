ebb7633c5d6bd4bb24c3a5483bd33fc74a78ace0
Ramiro Visca <ramiroviszan@gmail.com>
2018-05-06 20:16:52 -0300

    Event, call toggle image

--- src/app/components/pet-list/pet-list.component.ts
+++ src/app/components/pet-list/pet-list.component.ts
@@ -7,14 +7,18 @@ import { Pet } from '../../classes/pet';
 })
 export class PetListComponent {
 
-    @Input()pageTitle : string;
-    
+    @Input() pageTitle : string;
+    imageWidth: number = 100;
+    imageMargin: number = 1;
+    showImage: boolean = false;
+
     pets: Array<Pet> = [
         new Pet("1","Perro",4,"Grande", new Date(),20,"Golden Retriever", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Golden_Retriever_with_tennis_ball.jpg/1200px-Golden_Retriever_with_tennis_ball.jpg")
     ];
 
-    imageWidth: number = 100;
-    imageMargin: number = 1;
-
     constructor() {}
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
