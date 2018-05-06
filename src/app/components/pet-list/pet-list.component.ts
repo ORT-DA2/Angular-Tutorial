024d23608f2a19331b46618cd0587faf78a2d134
Ramiro Visca <ramiroviszan@gmail.com>
2018-05-06 18:16:10 -0300

    Filled the template with hardcoded pet data in the component

--- src/app/components/pet-list/pet-list.component.ts
+++ src/app/components/pet-list/pet-list.component.ts
@@ -1,14 +1,20 @@
 import {Component, Input} from '@angular/core';
+import { Pet } from '../../classes/pet';
 
 @Component({
     selector: 'pm-pets', 
     templateUrl: './pet-list.component.html'
 })
 export class PetListComponent {
+
+    @Input()pageTitle : string;
     
-    @Input()pageTitle : string = "Pet List";
-    @Input()pageTitlePassedAsProperty : string = "Pet List Subtitle";
-    @Input()pageTitleInterpolated : string = "Pet List Header";
+    pets: Array<Pet> = [
+        new Pet("1","Perro",4,"Grande", new Date(),20,"Golden Retriever", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Golden_Retriever_with_tennis_ball.jpg/1200px-Golden_Retriever_with_tennis_ball.jpg")
+    ];
+
+    imageWidth: number = 100;
+    imageMargin: number = 1;
 
     constructor() {}
 }
