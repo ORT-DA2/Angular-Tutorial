a1ad462e62a61a9ac17a8193101df45b9e52e14f
Santiago Mendez <santi17mendez@hotmail.com>
2018-05-29 21:34:47 -0300

    Basic Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { PetService } from './services/pet/pet.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PetService]
 })
 export class AppComponent {
   title = 'LUPI';
-  componentTitle:string = "Lista de Perros";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PetListComponent } from './components/pet-list/pet-list.component';
+import { PetsComponent } from './components/pets/pets.component';
 import { PetListFilterPipe } from './components/pet-list/pet-list-filter.pipe';
 import { StarRateComponent } from './components/star/star-rate.component';
 import { PetService } from './services/pet/pet.service';
-import { HttpModule } from '@angular/http';
+import { WelcomeComponent } from './components/home/welcome.component';
+import { importType } from '@angular/compiler/src/output/output_ast';
 
 @NgModule({
   declarations: [
     AppComponent,
     PetListComponent,
+    PetsComponent,
     PetListFilterPipe,
+    WelcomeComponent,
     StarRateComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'pets', component: PetsComponent},
+      { path: 'pets/:id', component: PetListComponent },
+      { path: 'welcome', component:  WelcomeComponent},
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   providers: [PetService],
   bootstrap: [AppComponent]
